3f8c7acf815df668ceda8c557a9489578af76695
MuskanBansal16 <54248197+MuskanBansal16@users.noreply.github.com>
2019-08-19 13:39:44 +0530

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,1 +1,21 @@
-
+def bubbleSort(arr): 
+    n = len(arr) 
+  
+     
+    for i in range(n): 
+      Swap=False
+  
+         
+        for j in range(0, n-i-1): 
+            
+            
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+                Swap=True
+            if(Swap==False):
+                break
+  
+ 
+arr=list(map(int,input().split()))  
+bubbleSort(arr) 
+print(arr)  
